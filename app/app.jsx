977bb3e40ce10b6d935eda5befeb0ccce6ae6171
Komputeros <komputeros@gmail.com>
2017-05-13 12:58:14 +0200

    first public release

--- app/app.jsx
+++ app/app.jsx
@@ -2,12 +2,14 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 //require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
 
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <p>Boilerplate3</p>,
+  <TodoApp />,
   document.getElementById('app')
 );
